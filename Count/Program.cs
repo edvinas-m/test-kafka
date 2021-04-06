9147d082fbd32d5db584ee4071583aff56bee2ba
emilutis <emilutis@lb.lt>
2021-04-06 20:23:16 +0300

    Add environment

--- Count/Program.cs
+++ Count/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-using System.Threading;
+using System.Threading.Tasks;
 
 namespace Count
 {
     class Program
     {
+        /*
         static void Main(string[] args)
         {
             long n = 0;
@@ -13,6 +14,35 @@ namespace Count
                 n++;
                 Console.WriteLine($"{DateTime.Now} : {n}");
                 Thread.Sleep(5000);
+            }
+        }
+        */
+        static async Task Main(string[] args)
+        {
+            long n = 0;
+            long max = -1;
+            if (args.Length > 0)
+            {
+                try
+                {
+                    max = Convert.ToInt64(args[0]);
+                }
+                catch
+                {
+                    max = -1;
+                }
+            }
+
+            var en = Environment.GetEnvironmentVariables().GetEnumerator();
+            while (en.MoveNext())
+            {
+                Console.WriteLine($"{en.Key} : {en.Value}");
+            }
+
+            while (max == -1 || n < max)
+            {
+                Console.WriteLine($"{DateTime.Now} : {++n}");
+                await Task.Delay(5000);
             }
         }
     }
