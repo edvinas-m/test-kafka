05899379ff4a68680588953de3aedc743781b1d6
emilutis <emilutis@lb.lt>
2021-04-06 16:43:17 +0300

    Count test

---
+++ Count/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = 0;
+            while(true)
+            {
+                n++;
+                Console.WriteLine($"{DateTime.Now} : {n}");
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
